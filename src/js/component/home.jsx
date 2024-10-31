9ce95c384a78a49d21220caeccca75a0948032ad
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-31 02:59:25 +0000

    avance lógica

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,16 +3,17 @@ import React, { useState } from "react";
 
 const Home = () => {
 	const [color, setColor] = useState(" ");
+	console.log(color);
 	return (
 		<div className="text-center">
-			<div>
-				<button type="button" class="btn btn-danger">Danger</button>
+			<div onClick={() => setColor("red")}>
+				<button type="button" className={"btn btn-danger" + " " + (color === "red" ? "sombra" : " ")}>Danger</button>
 			</div>
-			<div>
-				<button type="button" class="btn btn-warning">Warning</button>
+			<div onClick={() => setColor("yellow")}>
+				<button type="button" className={"btn btn-warning" + " " + (color === "yellow" ? "sombra" : " ")}>Warning</button>
 			</div>
-			<div>
-				<button type="button" class="btn btn-success">Success</button>
+			<div onClick={() => setColor("green")}>
+				<button type="button" className={"btn btn-success" + " " + (color === "green" ? "sombra" : " ")}>Success</button>
 			</div>
 		</div>
 	);
