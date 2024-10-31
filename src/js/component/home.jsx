ab6dff87a67795aa1e119739f3a76088e77c89f2
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-31 04:01:54 +0000

    Listo para enviar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,18 @@ const Home = () => {
 	const [color, setColor] = useState(" ");
 	console.log(color);
 	return (
-		<div className="container text-center bg-dark rounded">
-			<div onClick={() => setColor("red")} className="Figuras">
-				<button type="button" className={"btn btn-danger mt-4" + " " + (color === "red" ? "sombraRed" : "")}></button>
-			</div>
-			<div onClick={() => setColor("yellow")} className="Figuras">
-				<button type="button" className={"btn btn-warning mt-4" + " " + (color === "yellow" ? "sombraYellow" : " ")}></button>
-			</div>
-			<div onClick={() => setColor("green")} className="Figuras">
-				<button type="button" className={"btn btn-success mt-4" + " " + (color === "green" ? "sombraGreen" : " ")}></button>
+		<div>
+			<div className="rectangulo bg-dark text-center"></div>
+			<div className="container text-center bg-dark rounded">
+				<div onClick={() => setColor("red")} className="Figuras">
+					<button type="button" className={"btn btn-danger mt-4" + " " + (color === "red" ? "sombraRed" : "")}></button>
+				</div>
+				<div onClick={() => setColor("yellow")} className="Figuras" style={{ marginTop: '15px' }}>
+					<button type="button" className={"btn btn-warning mt-4" + " " + (color === "yellow" ? "sombraYellow" : " ")}></button>
+				</div>
+				<div onClick={() => setColor("green")} className="Figuras" style={{ marginTop: '15px' }}>
+					<button type="button" className={"btn btn-success mt-4" + " " + (color === "green" ? "sombraGreen" : " ")}></button>
+				</div>
 			</div>
 		</div>
 	);
